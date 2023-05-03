e6f5f098a8035f5cc2911a3022bf85820ac43777
ariaan ahmed <you@example.com>
2023-05-03 20:21:05 +0600

    active route added

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,6 +1,7 @@
 import React from 'react';
 import { Button, Container, Nav, Navbar } from 'react-bootstrap';
-import { Link } from 'react-router-dom';
+import { Link, NavLink } from 'react-router-dom';
+
 
 const Header = () => {
     return (
@@ -10,12 +11,12 @@ const Header = () => {
                 <Navbar.Toggle aria-controls="basic-navbar-nav" />
                 <Navbar.Collapse id="basic-navbar-nav">
                     <Nav className="ms-auto gap-lg-5 gap-3 d-lg-flex align-items-lg-center">
-                        <Link className='text-decoration-none fw-bold' to='/'>Home</Link>
-                        <Link className='text-decoration-none fw-bold' to='/blogs'>Blogs</Link>
-                        <Link className='text-decoration-none fw-bold' to='/about'>About</Link>
-                        <Link className='text-decoration-none' to='/login'>
+                        <NavLink to='/' className={({isActive }) => isActive ? 'active fw-bold text-decoration-none text-danger' : 'fw-bold text-decoration-none' }>Home</NavLink>
+                        <NavLink to='/blogs' className={({isActive }) => isActive ? 'active fw-bold text-decoration-none text-danger' : 'fw-bold text-decoration-none'}>Blogs</NavLink>
+                        <NavLink to='/about' className={({isActive }) => isActive ? 'active fw-bold text-decoration-none text-danger' : 'fw-bold text-decoration-none' }>About</NavLink>
+                        <NavLink className='text-decoration-none' to='/login'>
                             <Button className='fw-bold'>Login</Button>
-                        </Link>
+                        </NavLink>
                     </Nav>
                 </Navbar.Collapse>
             </Navbar >
