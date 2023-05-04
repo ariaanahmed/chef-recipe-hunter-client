cfbdf57f80b824648330978db19cc32f48bcb5f1
ariaan ahmed <you@example.com>
2023-05-04 08:58:32 +0600

    file from json file is not showing problem solved

--- src/components/recipes/Recipes.jsx
+++ src/components/recipes/Recipes.jsx
@@ -1,41 +1,22 @@
 import React from 'react';
-import { Button, Card, Col, Container, Row } from 'react-bootstrap';
-import { Rating } from '@smastrom/react-rating'
-import '@smastrom/react-rating/style.css'
-import { FaRegBookmark } from "react-icons/fa";
-import { toast } from 'react-toastify';
+import { Container } from 'react-bootstrap';
+import SingleRecipe from '../singleRecipe/SingleRecipe';
 
 const Recipes = ({ recipe }) => {
-
-    const recipesss = recipe.numbersOfrecipes;
-    const notify = () => toast("Item Saved!")
+    const recipes = recipe.numbersOfrecipes;
+    console.log(recipes)
 
     return (
         <Container className='mb-5'>
-            <Row>
-                <Col>
-                    <Card className='p-2' style={{ width: '100%' }}>
-                        <Card.Img variant="top" style={{ width: '100%', height: '200px' }} src={recipesss.recipe_1.recipe_image} />
-                        <Card.Body>
-                            <Card.Title>{recipesss.recipe_1.recipe_name}</Card.Title>
-                            <Card.Text>
-                                <span className='fw-bold'>Cooking Method:</span> {recipesss.recipe_1.recipe_description} <br />
-                                <span>
-                                    <span className='fw-semibold'>Ingredients: </span>{recipesss.recipe_1.ingredients}
-                                </span>
 
-                                <span className='d-lg-flex justify-content-between align-items-center'>
-                                    Rating:
-                                    <Rating style={{ maxWidth: '85px' }} value={recipesss.recipe_1.star_rating} readOnly />
-                                    
-                                    <span>{recipesss.recipe_1.star_rating}</span>
-                                    <Button onClick={notify}> <FaRegBookmark /> </Button>
-                                </span>
-                            </Card.Text>
-                        </Card.Body>
-                    </Card>
-                </Col>
-            </Row>
+            {
+                recipes.map((recipe, idx) => <SingleRecipe
+                    key={idx}
+                    recipe={recipe}
+                >
+
+                </SingleRecipe>)
+            }
         </Container>
     );
 };
